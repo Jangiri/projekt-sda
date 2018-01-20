c6a1df1c9d3b6ed42ffd24adb0e3dab70ff41f82
Krzysztof Krupa <krzysztof.s.krupa@gmail.com>
2018-01-20 11:40:09 +0100

    zmienilem kom2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("dowolny text - kolejnt teks");
-        System.out.println("dowolny - to tez teraz dodaje");
+        System.out.println("dowo");
         System.out.println("jeszcze jedno");
     }
 }
